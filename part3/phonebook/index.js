1e12f353da3a59921ec279d447b322c0b5bcda53
Faiq-ali567 <faiq.ali@7vals.com>
2026-05-31 19:21:56 +0500

    mongo db added in phonebook project

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
+const Phonebook = require('./models/phonebook')
+
 
 morgan.token('body', (request) => {
     return JSON.stringify(request.body)
@@ -13,72 +16,43 @@ app.use(express.json())
 app.use(morgan(':method :url :status :response-time ms - :body'))
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+    Phonebook.find({}).then(persons => {
+        response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/info', (request, response) => {
-    const personsCount = persons.length
-    const now = new Date()
-    response.send(`
-        <p>Phonebook has info for ${personsCount} people</p>
-        <p>${new Date()}</p>
-    `)
+app.get('/api/persons/info', (request, response, next) => {
+    Phonebook.find({}).then(persons => {
+        const personsCount = persons.length
+        const now = new Date()
+        response.send(`
+            <p>Phonebook has info for ${personsCount} people</p>
+            <p>${now}</p>
+        `)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    const person = persons.find((currentPerson) => currentPerson.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Phonebook.findById(id).then(person => {
+         if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Phonebook.findByIdAndDelete(id).then(() => response.status(204).end()).catch(error => next(error))
 })
 
-const generateID = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
-}
-
-const checkDuplicate = (name) => {
-    return persons.some(person => person.name === name)
-}
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -86,23 +60,52 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    if (checkDuplicate(body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        id: generateID(),
+    const person = new Phonebook({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons.push(person)
-    response.json(person)
+    person.save().then(savedPerson => response.json(savedPerson)).catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    const { name, number } = request.body
+
+    Phonebook.findById(id)
+        .then(person => {
+            if (!person) {
+                return response.status(404).end()
+            }
+
+            person.name = name
+            person.number = number
+            
+            person.save().then(savedPerson => response.json(savedPerson))
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://faiqnaeem24_db_user:${password}@cluster0.dbjg14b.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4 })
+
+const phonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const phonebookModel = mongoose.model('Phonebook', phonebookSchema)
+
+if (process.argv.length === 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    const person = new phonebookModel({
+        name: name,
+        number: number
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    phonebookModel.find({}).then(persons => {
+        persons.forEach(person => console.log(person))
+        mongoose.connection.close()
+    })
+}
